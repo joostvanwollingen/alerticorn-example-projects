e426bc7218e9edcef13e8bf7d8d9c7ddf67736e8
Joost van Wollingen <joostvanwollingen@gmail.com>
2025-02-05 22:49:40 +0100

    Add Java runWith example

--- java-junit/src/test/java/ExampleTest.java
+++ java-junit/src/test/java/ExampleTest.java
@@ -1,13 +1,18 @@
+import nl.vanwollingen.alerticorn.api.AlerticornMessage;
 import nl.vanwollingen.alerticorn.api.Message;
+import nl.vanwollingen.alerticorn.api.RunWith;
 import nl.vanwollingen.alerticorn.slack.Slack;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Message(title = "The default message")
 public class ExampleTest extends AlerticornTest {
 
-    private final String mySlackHook = "https://hooks.slack.com/services/<make>/<sure>/<to_replace>";
+    private final String mySlackHook = "https://hooks.slack.com/services/<remembe>/<to>/<replace>";
 
     @Slack
     @Test
@@ -22,5 +27,20 @@ public class ExampleTest extends AlerticornTest {
     @Message(title = "But you can override it")
     public void my_overriden_message_test() {
         assertEquals(false, true);
+    }
+
+    @Test
+    public void some_method() {
+        String platform = "slack";
+        String destination = mySlackHook;
+        String title = "Message Alert";
+
+        BiFunction<String, String, AlerticornMessage> buildMessage = (context, result) ->
+                new AlerticornMessage("Context: " + context + ", Result: " + result,null, null, null, null);
+
+        Supplier<String> block = () -> "Test message result";
+
+        String result = RunWith.INSTANCE.message(platform, destination, title, buildMessage::apply, block::get);
+        System.out.println("Result: " + result);
     }
 }
